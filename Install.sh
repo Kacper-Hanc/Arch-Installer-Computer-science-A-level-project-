a1457e447d0c291fbed00160d52ae3dc0a281ba9
Kacper-Hanc <KacperHanc@protonmail.com>
2026-03-21 18:16:49 +0000

    Update Install.sh such that selecting no does something
    Signed-off-by: Kacper-Hanc <KacperHanc@protonmail.com>

--- Install.sh
+++ Install.sh
@@ -1,10 +1,10 @@
 #gets the additional apps the user wants to install the uses pacstrap to install them
 apps=$(whiptail --title "Applications" --inputbox "Write in any apps to be installed" 15 25  3>&1 1>&2 2>&3)
 if (whiptail --title "GRUB" --yesno "Do you want grub as your bootmanager" 15 25 3>&1 1>&2 2>&3) then
-    echo pacstrap
     pacstrap /mnt linux base base-devel sof-firmware grub efibootmgr $apps
     grub=True
 else
+    pacstrap /mnt linux base base-devel sof-firmware $apps
     grub=False
 fi
 #copies necesary scirpts
@@ -13,7 +13,7 @@ chmod +x /mnt/Config.sh
 #generates the fstab file. Important for auto mounting drives during boot sequence
 genfstab /mnt >> /mnt/etc/fstab
 cat /mnt/etc/fstab
-arch-chroot /mnt bash Config.sh $1 $2
+arch-chroot /mnt bash Config.sh $1 $2 $grub
 
 umount -a 
 swapoff /dev/$1
